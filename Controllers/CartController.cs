8da8b44f59a907f3d1b1ccb82beae53f8b36bf60
SamYao <samyao779@gmail.com>
2015-07-31 04:35:43 +0700

    Day la code demo ShoppingCart using Session

---
+++ Controllers/CartController.cs
@@ -0,0 +1,57 @@
+﻿using DemoShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoShoppingCart.Controllers
+{
+    public class CartController : Controller
+    {
+        //
+        // GET: /Cart/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add(int? Id)
+        {
+            ShoppingCart.Cart.Add(Id);
+            var info = new
+            {
+                ShoppingCart.Cart.Count,
+                ShoppingCart.Cart.Amout
+            };
+            return Json(info, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Update(int? Id)
+        {
+            foreach (var p in ShoppingCart.Cart.Items)
+            {
+                var id = p.Id.ToString();
+                p.Quantity = int.Parse(Request[id]);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Remove(int? Id)
+        {
+            ShoppingCart.Cart.Remove(Id);
+            var info = new
+            {
+                ShoppingCart.Cart.Count,
+                ShoppingCart.Cart.Amout
+            };
+            return Json(info, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Clear()
+        {
+            ShoppingCart.Cart.Clear();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/ShoppingCart.cs
@@ -0,0 +1,86 @@
+﻿using DemoShoppingCart.Models.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoShoppingCart.Models
+{
+    public class ShoppingCart
+    {
+        public static ShoppingCart Cart
+        {
+            get
+            {
+                var cart = HttpContext.Current.Session["Cart"] as ShoppingCart;
+                if (cart == null)
+                {
+                    cart = new ShoppingCart();
+                    HttpContext.Current.Session["Cart"] = cart;
+                }
+                return cart;
+            }
+        }
+
+        public List<Product> Items = new List<Product>();
+
+        public int Count
+        {
+            get
+            {
+                if (Items.Count > 0)
+                {
+                    return Items.Sum(p => p.Quantity);
+                }
+                return 0;
+            }
+        }
+
+        public double Amout
+        {
+            get
+            {
+                if (Items.Count > 0)
+                {
+                    return Items.Sum(p => p.Quantity * p.UnitPrice);
+                }
+                return 0;
+            }
+        }
+
+        public void Add (int? Id)
+        {
+            try
+            {
+                var prod = Items.Single(p => p.Id == Id);
+                prod.Quantity++;
+            }
+            catch
+            {
+                using (var db = new ShoppingCartDbContext())
+                {
+                    var prod = db.Products.Find(Id);
+                    prod.Quantity = 1;
+                    Items.Add(prod);
+                }
+            }
+        }
+
+        public void Update(int? Id, int NewQuantity)
+        {
+            var prod = Items.Single(p => p.Id == Id);
+            prod.Quantity = NewQuantity;
+        }
+
+        public void Remove(int? Id)
+        {
+            var prod = Items.Single(p => p.Id == Id);
+            Items.Remove(prod);
+        }
+
+        public void Clear()
+        {
+            Items.Clear();
+        }
+    }
+}
